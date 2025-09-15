2e2f0a52d6a9a5f52d52af735dc5a0f9c85d1ed1
zhenklchhh <zaleshchenok.e@mail.ru>
2025-09-15 12:15:50 +0300

    feat: eliminates hardcoded magic numbers, robot requirements can now be modified in a single location.

--- skynet/src/main/java/com/innowise/Faction.java
+++ skynet/src/main/java/com/innowise/Faction.java
@@ -4,6 +4,7 @@ import com.innowise.factory.FactorySaleManager;
 import com.innowise.simulation.Simulation;
 import com.innowise.util.RandomUtil;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,6 +16,9 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @author Evgeniy Zaleshchenok
  */
 public class Faction extends Thread {
+    private static final Map<RobotParts, Integer> ROBOT_BLUEPRINT;
+    private static final int TOTAL_PARTS_PER_ROBOT;
+
     private int quantityOfRobots = 0;
     private String factionName;
     private FactorySaleManager factorySaleManager;
@@ -24,12 +28,24 @@ public class Faction extends Thread {
     private final AtomicInteger daysCounter;
     private final RandomUtil randomUtil = new RandomUtil();
 
+    static {
+        Map<RobotParts, Integer> blueprint = new HashMap<>();
+        blueprint.put(RobotParts.HEAD, 1);
+        blueprint.put(RobotParts.TORSO, 1);
+        blueprint.put(RobotParts.HAND, 2);
+        blueprint.put(RobotParts.FEET, 2);
+        ROBOT_BLUEPRINT = Collections.unmodifiableMap(blueprint);
+        TOTAL_PARTS_PER_ROBOT = ROBOT_BLUEPRINT.values().stream()
+                .mapToInt(Integer::intValue).sum();
+    }
+
     public Faction(String factionName, FactorySaleManager factorySaleManager, AtomicInteger daysCounter, CyclicBarrier cyclicBarrier) {
         this.factionName = factionName;
         this.factorySaleManager = factorySaleManager;
         this.daysCounter = daysCounter;
         this.cyclicBarrier = cyclicBarrier;
     }
+
 
     @Override
     public void run() {
@@ -57,19 +73,30 @@ public class Faction extends Thread {
     }
 
     private void buildRobots() {
-        boolean hasRequiredDetails = true;
-        while (hasRequiredDetails) {
-            if (robotPartsWarehouse.getOrDefault(RobotParts.FEET, 0) > 1 && robotPartsWarehouse.getOrDefault(RobotParts.HAND, 0) > 1 &&
-                    robotPartsWarehouse.getOrDefault(RobotParts.TORSO, 0) > 0 && robotPartsWarehouse.getOrDefault(RobotParts.HEAD, 0) > 0) {
-                robotPartsWarehouse.put(RobotParts.FEET, robotPartsWarehouse.get(RobotParts.FEET) - 2);
-                robotPartsWarehouse.put(RobotParts.HAND, robotPartsWarehouse.get(RobotParts.HAND) - 2);
-                robotPartsWarehouse.put(RobotParts.TORSO, robotPartsWarehouse.get(RobotParts.TORSO) - 1);
-                robotPartsWarehouse.put(RobotParts.HEAD, robotPartsWarehouse.get(RobotParts.HEAD) - 1);
-                quantityOfRobots++;
-                amountOfParts.addAndGet(-6);
-            } else {
-                hasRequiredDetails = false;
+        while (canBuildRobot()) {
+            consumePartsForRobot();
+            quantityOfRobots++;
+            amountOfParts.addAndGet(-TOTAL_PARTS_PER_ROBOT);
+        }
+    }
+
+    private boolean canBuildRobot() {
+        for (Map.Entry<RobotParts, Integer> requiredPart : ROBOT_BLUEPRINT.entrySet()) {
+            RobotParts partType = requiredPart.getKey();
+            int requiredAmount = requiredPart.getValue();
+            int availableAmount = robotPartsWarehouse.getOrDefault(partType, 0);
+            if (availableAmount < requiredAmount) {
+                return false;
             }
+        }
+        return true;
+    }
+
+    private void consumePartsForRobot() {
+        for (Map.Entry<RobotParts, Integer> requiredPart : ROBOT_BLUEPRINT.entrySet()) {
+            RobotParts partType = requiredPart.getKey();
+            int requiredAmount = requiredPart.getValue();
+            robotPartsWarehouse.compute(partType, (key, currentAmount) -> currentAmount - requiredAmount);
         }
     }
 
